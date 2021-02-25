defa865e075152c7658d5b736cfa13176b95a972
Ben Hyrman <ben.hyrman@gmail.com>
2021-02-24 22:02:46 -0500

    Remove gcp-specific port binding stuff

--- SharpStatusApp/Program.cs
+++ SharpStatusApp/Program.cs
@@ -19,13 +19,10 @@ namespace SharpStatusApp
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
-            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            var url = $"http://0.0.0.0:{port}";
-
             return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>().UseUrls(url);
+                    webBuilder.UseStartup<Startup>();
                 });
         }
     }

11e6a41bac798057e78041761559eb6e977c7c04
Ben Hyrman <ben@hyr.mn>
2021-03-24 00:54:57 -0400

    Move the db migration stuff to a separate file

---
+++ SharpStatusApp/IHostDbExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using SharpStatusApp.Data;
+
+namespace SharpStatusApp
+{
+    internal static class IHostDbExtensions
+    {
+        internal static IHost MigrateDatabases(this IHost host) =>
+            host.MigrateOrganizationDb();
+
+        internal static IHost MigrateOrganizationDb(this IHost host) => 
+            host.MigrateDbContext<ApplicationUserDbContext>();
+
+        internal static IHost MigrateDbContext<TContext>(this IHost host) where TContext : DbContext
+        {
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            var logger = services.GetRequiredService<ILogger<TContext>>();
+            using var dbCtx = services.GetRequiredService<TContext>();
+
+            ValidateModelSnapshot(logger, dbCtx);
+
+            RunMigrations(logger, dbCtx);
+
+            return host;
+
+            static void ValidateModelSnapshot(ILogger<TContext> logger, TContext dbCtx)
+            {
+                logger.LogInformation("Validating model snapshot for {DbContext}", typeof(TContext).Name);
+
+                var migrationsAssembly = dbCtx.GetService<IMigrationsAssembly>();
+                var snapshot = migrationsAssembly.ModelSnapshot.Model;
+
+                var dependencies = dbCtx.GetService<ProviderConventionSetBuilderDependencies>();
+                var relationalDependencies = dbCtx.GetService<RelationalConventionSetBuilderDependencies>();
+
+                var typeMappingConvention = new TypeMappingConvention(dependencies);
+                typeMappingConvention.ProcessModelFinalizing(((IConventionModel)snapshot).Builder, null);
+
+                var relationalModelConvention = new RelationalModelConvention(dependencies, relationalDependencies);
+                var sourceModel = relationalModelConvention.ProcessModelFinalized(snapshot);
+
+                var modelDiffer = dbCtx.GetService<IMigrationsModelDiffer>();
+                var hasDifferences = modelDiffer.HasDifferences(
+                    sourceModel.GetRelationalModel(),
+                    dbCtx.Model.GetRelationalModel());
+
+                if (hasDifferences)
+                {
+                    logger.LogCritical("There are differences between the current database model for {DbContext} and the most recent migration.", typeof(TContext).Name);
+                    throw new InvalidOperationException($"There are differences between the current database model for {typeof(TContext).Name} and the most recent migration.");
+                }
+            }
+
+            static void RunMigrations(ILogger<TContext> logger, TContext dbCtx)
+            {
+                logger.LogInformation("Running database migrations for context {DbContext}", typeof(TContext).Name);
+                var migrations = dbCtx.Database.GetPendingMigrations().ToList();
+
+                if (migrations.Count == 0)
+                {
+                    logger.LogInformation("No pending migrations");
+                }
+                else
+                {
+                    var migrator = dbCtx.GetService<IMigrator>();
+
+                    foreach (var migration in migrations)
+                    {
+                        logger.LogInformation("Applying migration {Migration}", migration);
+
+                        migrator.Migrate(migration);
+                    }
+                }
+
+                logger.LogInformation("Database migration complete");
+            }
+        }
+    }
+}
--- SharpStatusApp/Program.cs
+++ SharpStatusApp/Program.cs
@@ -1,21 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions;
-using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-using SharpStatusApp.Data;
 
 namespace SharpStatusApp
 {
@@ -24,36 +8,7 @@ namespace SharpStatusApp
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            
-/*            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-
-            using var userDbCtx = services.GetRequiredService<ApplicationUserDbContext>();
-            var migrationsAssembly = userDbCtx.GetService<IMigrationsAssembly>();
-
-            var snapshot = migrationsAssembly.ModelSnapshot.Model;
-
-            var dependencies = userDbCtx.GetService<ProviderConventionSetBuilderDependencies>();
-            var relationalDependencies = userDbCtx.GetService<RelationalConventionSetBuilderDependencies>();
-
-            var typeMappingConvention = new TypeMappingConvention(dependencies);
-            typeMappingConvention.ProcessModelFinalizing(((IConventionModel)snapshot).Builder, null);
-
-            var relationalModelConvention = new RelationalModelConvention(dependencies, relationalDependencies);
-            var sourceModel = relationalModelConvention.ProcessModelFinalized(snapshot);
-
-            var modelDiffer = userDbCtx.GetService<IMigrationsModelDiffer>();
-            var hasDifferences = modelDiffer.HasDifferences(
-                sourceModel.GetRelationalModel(),
-                userDbCtx.Model.GetRelationalModel());
-
-            if (hasDifferences)
-            {
-                throw new InvalidOperationException("There are differences between the current database model and the most recent migration.");
-            }
-
-            userDbCtx.Database.Migrate();
-*/
+            host.MigrateDatabases();
             host.Run();
         }
 
